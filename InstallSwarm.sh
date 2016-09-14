45b4fa2c77c23a5ff5a24fbc8ebb31fec0fe32ad
Mark Peng <markpeng@outlook.com>
2016-09-14 20:38:28 +0800

    Revert "Revert "Swarm native mode""
    This reverts commit 73827214c82a565afa718230e54738668419a906.

---
+++ InstallSwarm.sh
@@ -0,0 +1,13 @@
+# Installing Docker Machine
+sudo curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+chmod +x /usr/local/bin/docker-machine
+
+# Define the number of managers/workers
+MANAGER=1
+WORKER=3
+
+# Init the swarm
+sudo docker-machine ssh swarm-master-1 docker swarm init --auto-accept manager --auto-accept worker --listen-addr $(docker-machine ip swarm-master-0):2377
+
+# Add workers
+for i in $(seq 0 $WORKER); do sudo docker-machine ssh swarm-node-$i docker swarm join --listen-addr $(docker-machine ip swarm-node-$i):2377 $(docker-machine ip swarm-master-0):2377; done
