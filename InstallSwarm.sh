39f375369ba3d37924de2eefe9df8b8a0e15cb34
Mark Peng <markpeng@outlook.com>
2016-09-14 21:10:55 +0800

    Modify bash

--- InstallSwarm.sh
+++ InstallSwarm.sh
@@ -7,7 +7,7 @@ MANAGER=1
 WORKER=3
 
 # Init the swarm
-sudo docker-machine ssh swarm-master-1 docker swarm init --auto-accept manager --auto-accept worker --listen-addr $(docker-machine ip swarm-master-0):2377
+sudo docker-machine ssh swarm-master-0 docker swarm init --auto-accept manager --auto-accept worker --listen-addr $(docker-machine ip swarm-master-0):2377
 
 # Add workers
 for i in $(seq 0 $WORKER); do sudo docker-machine ssh swarm-node-$i docker swarm join --listen-addr $(docker-machine ip swarm-node-$i):2377 $(docker-machine ip swarm-master-0):2377; done
