e2a4216f72f38ccecafe82a2896bb0f7bc6a5c49
Mark Peng <markpeng@outlook.com>
2016-09-20 15:01:54 +0800

    Sleep

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -3,3 +3,4 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get install -y docker-engine=1.11*
+sleep 1m
