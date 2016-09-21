5440611ea19bca1740f64de097c5403e13aba835
ahpeng <markpeng@outlook.com>
2016-09-21 21:48:34 +0800

    Update InstallDocker.sh

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -5,5 +5,3 @@ sudo apt-get update
 sudo apt-get install -y docker-engine=1.11*
 sudo curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://4d0183dd.m.daocloud.io
 sudo service docker restart
-docker pull progrium/consul
-docker pull swarm
